2531ada507daa73c0bd8129d6eef7af54ce489eb
Suriya Sundar <ssampa7@uic.edu>
2015-11-08 20:24:44 -0600

    testing

--- sentiment.py
+++ sentiment.py
@@ -45,7 +45,7 @@ neg_tweets = [('I do not like this car', 'negative'),
 training_data_workbook = openpyxl.load_workbook("training-Obama-Romney-tweets.xlsx")
 sheet_obama = training_data_workbook.get_sheet_by_name("Obama")
 tweets = []
-for i in range(3,7201):
+for i in range(3,7000):
     try:
         tweet_string = str(sheet_obama.cell(row=i,column=4).value)
         tweet_class = class_value_mapping[int(sheet_obama.cell(row=i,column=5).value)]
@@ -60,9 +60,18 @@ word_features = get_word_features(get_words_in_tweets(tweets))
 training_set = nltk.classify.apply_features(extract_features, tweets)
 #pprint(word_features)
 classifier = nltk.NaiveBayesClassifier.train(training_set)
+test_tweet_list = []
+for i in range(7001,7201):
+    try:
+        tweet_string = str(sheet_obama.cell(row=i,column=4).value)
+        tweet_class = class_value_mapping[int(sheet_obama.cell(row=i,column=5).value)]
+        tweet_string = clean_test_tweets([tweet_string])[0]
+        test_tweet_list.append((tweet_string, tweet_class))
+    except Exception:
+        pass
 
-tweet = 'enemy view'
-clean_tweet = clean_test_tweets([tweet])
 
-for x in clean_tweet:
-  print classifier.classify(extract_features(x))
+#clean_tweet = clean_test_tweets([tweet])
+
+for (tweet, class_type) in test_tweet_list:
+  print tweet, class_type, classifier.classify(extract_features(x))
